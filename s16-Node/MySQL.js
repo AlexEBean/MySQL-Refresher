6ba2cb1f423757752d47bd13218674b640c35f3a
Alex <alexbean95@gmail.com>
2021-08-18 18:51:31 -0600

    Beginning to break down connecting MySQL to Nodejs

---
+++ s16-Node/MySQL.js
@@ -0,0 +1,25 @@
+// Run npm install mysql in bash
+
+const mysql = require('mysql');
+ 
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',     // your root username
+  database : 'join_us'   // the name of your db
+});
+
+
+// This is effectively SELECT 1 + 1
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+ })
+
+ // This is effectively SELECT CURTIME() as time, CURDATE() as date, NOW() as now
+ const q = 'SELECT CURTIME() as time, CURDATE() as date, NOW() as now';
+ connection.query(q, function (error, results, fields) {
+   if (error) throw error;
+   console.log(results[0].time);
+   console.log(results[0].date);
+   console.log(results[0].now);
+ });
---
+++ s16-Node/faker.js
@@ -0,0 +1,29 @@
+// for(let i = 0; i < 500; i++){
+//     console.log("HELLO WORLD!");
+//   }
+
+// node s16-Node/app.js
+
+// Run npm i faker in bash
+
+const faker = require('faker')
+
+// Print a random email
+
+console.log(faker.internet.email()); 
+
+// Print a random past date
+
+console.log(faker.date.past().toString());
+
+// Print a random city
+
+console.log(faker.address.city()); 
+
+function generateAddress(){
+  console.log(faker.address.streetAddress());
+  console.log(faker.address.city());
+  console.log(faker.address.state());
+}
+
+generateAddress(); 
